1c653fff00296c53597d51ac10ba5f79f8cab370
KevinWu532 <wujing532@outlook.com>
2019-01-04 18:44:21 +0800

    add challenge 15

---
+++ challenge_15/shiyanlou-github-repositories.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import scrapy
+
+class ShiyanlouGithub(scrapy.Spider):
+    name = 'shiyanlou-github-repositories'
+
+    def start_requests(self):
+        temp = [
+                'https://github.com/shiyanlou?tab=repositories',
+                'https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK5MjAxNy0wNi0wN1QwNjoyMToxMCswODowMM4FkpVn&tab=repositories',
+                'https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK5MjAxNS0wMS0yNlQxMTozMDoyNSswODowMM4Bx2JQ&tab=repositories',
+                'https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK5MjAxNC0xMS0yMVQxODowOTowMiswODowMM4BnQBZ&tab=repositories'
+                ]
