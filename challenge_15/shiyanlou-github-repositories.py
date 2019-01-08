eec1b35e28c72c0d5d7b81d0c5c7298f22365a2e
KevinWu532 <wujing532@outlook.com>
2019-01-09 06:44:08 +0800

    爬取实验楼github

--- challenge_15/shiyanlou-github-repositories.py
+++ challenge_15/shiyanlou-github-repositories.py
@@ -6,9 +6,20 @@ class ShiyanlouGithub(scrapy.Spider):
     name = 'shiyanlou-github-repositories'
 
     def start_requests(self):
-        temp = [
+        temp = (
                 'https://github.com/shiyanlou?tab=repositories',
                 'https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK5MjAxNy0wNi0wN1QwNjoyMToxMCswODowMM4FkpVn&tab=repositories',
                 'https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK5MjAxNS0wMS0yNlQxMTozMDoyNSswODowMM4Bx2JQ&tab=repositories',
                 'https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK5MjAxNC0xMS0yMVQxODowOTowMiswODowMM4BnQBZ&tab=repositories'
-                ]
+                )
+        for url_temp in temp:
+            urls = (url_temp.format(i) for i in range(1,31))
+            for url in urls:
+                yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        for repositories in response.css('div[class="col-9 d-inline-block"]'):
+            yield {
+                'name':repositories.xpath('.//h3/a/text()').re_first('\n        (.+)'),
+                'update_time':repositories.xpath('.//relative-time/@datetime').extract_first()
+            }
