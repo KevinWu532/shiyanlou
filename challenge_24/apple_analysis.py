b0ea2a041f04322e03d76ede8c0b7ad6992a6739
KevinWu532 <wujing532@outlook.com>
2019-01-23 03:41:47 +0800

    精简

--- challenge_24/apple_analysis.py
+++ challenge_24/apple_analysis.py
@@ -2,9 +2,12 @@ import pandas as pd
 
 def quarter_volume():
     data = pd.read_csv("apple.csv",header=0)
+    '''
     data = data.set_index(pd.to_datetime(data['Date']))
     data = data.resample('Q').sum()
     second_volume = data.sort_values(by='Volume',ascending=False)['Volume'][1]
+    '''
+    second_volume = data.set_index(pd.to_datetime(data['Date'])).resample('Q').sum().sort_values(by='Volume',ascending=False)['Volume'][1]
     return second_volume
 
 if __name__ == '__main__':
