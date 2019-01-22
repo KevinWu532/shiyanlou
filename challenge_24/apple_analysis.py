e4e8fac7455083ab552d1b25145498edf4df020f
KevinWu532 <wujing532@outlook.com>
2019-01-23 03:34:44 +0800

    pandas 按时间序列，重采样，排序

---
+++ challenge_24/apple_analysis.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def quarter_volume():
+    data = pd.read_csv("apple.csv",header=0)
+    data = data.set_index(pd.to_datetime(data['Date']))
+    data = data.resample('Q').sum()
+    second_volume = data.sort_values(by='Volume',ascending=False)['Volume'][1]
+    return second_volume
+
+if __name__ == '__main__':
+    print(quarter_volume())
